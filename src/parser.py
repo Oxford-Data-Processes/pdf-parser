c4a2855c2e52e9f50cdbb663c2474a5d24b6415a
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-26 20:31:43 +0000

    Checkpoint

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,36 @@
-class FormParser:
+from typing import List
+
+
+class Parser:
+
+    def page_number_converter(
+        self, page_numbers: str, number_of_pages: int
+    ) -> List[int]:
+        if ":" in page_numbers:
+            left_index = int(page_numbers.split(":")[0])
+            right_index = int(page_numbers.split(":")[1])
+        else:
+            index = int(page_numbers)
+            if index >= 0:
+                index = index - 1
+            elif index < 0:
+                index = number_of_pages + index
+            return [index]
+
+        if left_index > 0:
+            left_index -= 1
+        if right_index > 0:
+            right_index -= 1
+
+        if left_index < 0:
+            left_index = number_of_pages + left_index + 1
+        if right_index < 0:
+            right_index = number_of_pages + right_index + 1
+
+        if left_index == right_index:
+            return [left_index]
+
+        return list(range(left_index, right_index))
 
     def get_rule_from_id(self, rule_id, template):
         return [item for item in template["rules"] if item["rule_id"] == rule_id][0]
@@ -27,7 +59,9 @@ class FormParser:
         )
         return self.get_text_from_items(items_within_coordinates)
 
-    def get_output_data_for_rule(self, form_rule_id, page_index, pdf_data, template):
+    def get_output_data_from_form_rule(
+        self, form_rule_id, page_index, pdf_data, template
+    ):
         form_rule = self.get_rule_from_id(form_rule_id, template)
         coordinates = form_rule["config"]["coordinates"]
         page_content = pdf_data["pages"][page_index]["content"]
