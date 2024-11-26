a06096c3b90611e55b68160290f62c90316425d4
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-26 18:20:38 +0000

    Checkpoint

--- src/parser.py
+++ src/parser.py
@@ -1,25 +1,33 @@
-import json
-import os
-from typing import Dict, Any
-from datetime import datetime
+class FormParser:
+    def get_items_in_bounding_box(self, page_data, coordinates):
+        items_in_box = []
+        for item in page_data:
+            bounding_box = item["bounding_box"]["decimal_coordinates"]
+            if (
+                bounding_box["top_left"]["x"] >= coordinates["top_left"]["x"]
+                and bounding_box["top_left"]["y"] >= coordinates["top_left"]["y"]
+                and bounding_box["bottom_right"]["x"]
+                <= coordinates["bottom_right"]["x"]
+                and bounding_box["bottom_right"]["y"]
+                <= coordinates["bottom_right"]["y"]
+            ):
+                items_in_box.append(item)
+        return items_in_box
 
+    def get_text_from_items(self, items):
+        return "".join([item["text"] for item in items])
 
-class PDFParser:
-    def parse(
-        self, template: Dict[str, Any], pdf_data: Dict[str, Any]
-    ) -> Dict[str, Any]:
-        pass
+    def get_text_from_page(self, page_content, coordinates):
+        items_within_coordinates = self.get_items_in_bounding_box(
+            page_content, coordinates
+        )
+        return self.get_text_from_items(items_within_coordinates)
 
-
-template_path: str = os.path.join("src", "templates", "barclays_template.json")
-pdf_data_path: str = os.path.join("src", "pdf_data", "barclays_pdf_data.json")
-
-template: Dict[str, Any] = json.load(open(template_path))
-pdf_data: Dict[str, Any] = json.load(open(pdf_data_path))
-
-parser: PDFParser = PDFParser()
-output: Dict[str, Any] = parser.parse(template, pdf_data)
-
-print(output)
-
-assert output == json.load(open("src/outputs/barclays_output.json"))
+    def get_output_data_for_rule(self, form_rule, page_number, pdf_data):
+        coordinates = form_rule["config"]["coordinates"]
+        page_content = pdf_data["pages"][page_number - 1]["content"]
+        return {
+            form_rule["config"]["field_name"]: self.get_text_from_page(
+                page_content, coordinates
+            )
+        }
