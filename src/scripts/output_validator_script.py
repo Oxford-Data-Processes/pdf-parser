fc58c1dd666d4e50894bb80e6f7de14c615cd41b
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-12-08 10:03:17 +0000

    templates are valid

--- src/scripts/output_validator_script.py
+++ src/scripts/output_validator_script.py
@@ -1,20 +1,30 @@
 import json
 from jsonschema import validate
-
-template_name = "barclays_student"
-identifier = "march"
-
-template = json.load(open(f"src/templates/{template_name}_template.json"))
-
-
-output_path = f"src/outputs/{template_name}_{identifier}_output.json"
-with open("template_json_schema.json") as schema_file:
-    schema = json.load(schema_file)
-
-validate(instance=template, schema=schema)
-
-generated_output = json.load(open(output_path))
-
 from pydantic_models import Document
 
-Document(**generated_output)
+templates_and_identifiers = {
+    "barclays_student": ["march", "april", "may"],
+    "barclays": ["march", "april", "may"],
+    "first_direct": ["march", "april", "may"],
+    "halifax": ["march", "april", "may"],
+    "lloyds": ["september"],
+    "monzo": ["november", "3_months"],
+    "payslip": ["jake"],
+}
+
+for template_name, identifiers in templates_and_identifiers.items():
+    for identifier in identifiers:
+        template = json.load(open(f"src/templates/{template_name}_template.json"))
+
+        with open("template_json_schema.json") as schema_file:
+            schema = json.load(schema_file)
+
+        print("TEMPLATE")
+        print(template_name, identifier)
+
+        validate(instance=template, schema=schema)
+
+        output_path = f"src/outputs/{template_name}_{identifier}_output.json"
+        generated_output = json.load(open(output_path))
+
+        Document(**generated_output)
