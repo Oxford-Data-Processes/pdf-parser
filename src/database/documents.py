49cb8bc76a7839f9b01f72a31498084764baf203
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-12-13 17:51:33 +0000

    Building database 4

--- src/database/documents.py
+++ src/database/documents.py
@@ -1,18 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, Field
 from typing import List
 from shared_models import Id, Datetime
 from enum import Enum
 
 
 class DocumentStatus(str, Enum):
+    PENDING = "PENDING"
+    PROCESSING = "PROCESSING"
     PROCESSED = "PROCESSED"
-    FAILED = "FAILED"
+    ERROR = "ERROR"
+
+
+class DocumentType(str, Enum):
+    MORTGAGE_APPLICATION = "Mortgage Application"
+    BANK_STATEMENT = "Bank Statement"
+    PAY_STUB = "Pay Stub"
+    TAX_RETURN = "Tax Return"
+    ID_DOCUMENT = "ID Document"
+    OTHER = "Other"
+
+
+class PDFName(constr):
+    __root__: str = Field(..., max_length=255, regex=r"\.pdf$")
 
 
 class Document(BaseModel):
     id: Id
     client_id: Id
-    name: str
+    name: PDFName
     type: str
     status: DocumentStatus
     file_path: str
